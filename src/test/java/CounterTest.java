4951828720f351ca55040dee57508001b44fc264
Foxxie1440 <90520474+ZhafranHafizh@users.noreply.github.com>
2024-05-15 20:28:19 +0700

    Update CounterTest.java
    Mengubah Expected value dari minus ke plus

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -35,7 +35,7 @@ public class CounterTest {
 		
 		for(int i = 1; i <10; i++) {
 			testCounter.decrement();
-			assertEquals(testCounter.getCount(), i * -1);
+			assertEquals(testCounter.getCount(), i * 1);
 		}
 	}
 
